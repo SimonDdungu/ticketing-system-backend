3f53dbb1812cc4857f6d1b7f0202d9a0cd0bcfb0
SimonDdungu <dev.simonddungu@gmail.com>
2026-04-17 10:39:18 +0300

    refactor: add Event, TicketType to DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,4 +8,8 @@ public class AppDbContext: DbContext
     
     public DbSet<User> Users => Set<User>();
     public DbSet<Organizer> Organizers => Set<Organizer>();
+
+    public DbSet<Event> Events => Set<Event>();
+
+    public DbSet<TicketType> TicketTypes => Set<TicketType>();
 }
