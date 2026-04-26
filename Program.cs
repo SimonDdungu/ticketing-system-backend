ad7bec3edc9318b25f3d564054fc43c7648c3709
SimonDdungu <dev.simonddungu@gmail.com>
2026-04-26 17:58:05 +0300

    Registered all Services and Repositories

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Ticketing_backend.Repositories.Interfaces;
 using Ticketing_backend.Repositories.Implementations;
 using Ticketing_backend.Services.Interfaces;
 using Ticketing_backend.Services.Implementations;
+using Ticketing_backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,11 +26,43 @@ builder.Services.AddIdentity<User, IdentityRole<Guid>>()
     .AddDefaultTokenProviders();
 
 
+// Elastic Search
+builder.Services.AddSingleton<ElasticsearchClient>(sp =>
+{
+    var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
+        .DefaultIndex("events");
+
+    return new ElasticsearchClient(settings);
+});
+
+
+
+
+// Repositories
 builder.Services.AddScoped<IOrganizerRepository, OrganizerRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventImageRepository, EventImageRepository>();
+builder.Services.AddScoped<ITicketTypeRepository, TicketTypeRepository>();
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+
+// Services
 builder.Services.AddScoped<IOrganizerService, OrganizerService>();
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEventImageService, EventImageService>();
+builder.Services.AddScoped<ITicketTypeService, TicketTypeService>();
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+
+
+
+
 var app = builder.Build();
+
+
 
 // Seed The Role and Permission Database.
 using (var scope = app.Services.CreateScope())
@@ -40,14 +73,6 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-
-builder.Services.AddSingleton<ElasticsearchClient>(sp =>
-{
-    var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
-        .DefaultIndex("events");
-
-    return new ElasticsearchClient(settings);
-});
 
 
 app.UseHttpsRedirection();
